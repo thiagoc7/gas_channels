1e7f736f2105e26173d9a9c35497018e511db67c
Thiago Correa <thiagobocorrea@gmail.com>
2016-01-18 17:35:40 -0200

    hacks for redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use ActionCableGas
+  config.web_socket_server_url = "ws://104.131.167.51/"
 end
